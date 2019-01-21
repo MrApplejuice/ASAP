76b77c50060175698199bb07c13474a8d144a42b
Paul Konstantin Gerke <paulkgerke@craftware.info>
2019-01-21 21:37:21 +0100

    Implemented building of python wrappers
    Issue #2

--- buildtools/docker/build.sh
+++ buildtools/docker/build.sh
@@ -5,11 +5,15 @@ set -e
 mkdir -p /build
 cd /build
 
+python3.7 -m pip install numpy
+
 cmake -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_ASAP:BOOL=ON \
     -DBUILD_EXECUTABLES:BOOL=ON \
     -DBUILD_IMAGEPROCESSING:BOOL=ON \
     -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT:BOOL=ON \
+    -DWRAP_WHOLESLIDEFILTERS_PYTHON=ON \
+    -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=ON \
     /src
 
 cpu_count=$(cat /proc/cpuinfo  | grep ^processor | wc -l)
